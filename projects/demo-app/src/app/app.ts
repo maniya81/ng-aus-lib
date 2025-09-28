12af215d5475e2197de169c0d55fe2e53b507aab
Sagar Maniya <maniyasagar26@gmail.com>
2025-09-28 16:56:44 +1000

    Add ABN input field with validation to demo form

--- projects/demo-app/src/app/app.ts
+++ projects/demo-app/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { JsonPipe } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { JsonPipe, NgIf } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { abnValidator } from 'ng-aus-lib/validators';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ReactiveFormsModule, JsonPipe],
+  imports: [RouterOutlet, ReactiveFormsModule, JsonPipe, NgIf],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
@@ -13,7 +14,15 @@ export class App {
   protected readonly title = signal('ng-aus-lib Demo');
   protected readonly demoForm = new FormGroup({
     sampleText: new FormControl<string>('', { nonNullable: true }),
+    abn: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required, abnValidator()],
+    }),
   });
+
+  protected get abnControl(): FormControl<string> {
+    return this.demoForm.get('abn') as FormControl<string>;
+  }
 
   protected onSubmit(): void {
     if (this.demoForm.invalid) {
