11d95a7866501dac925a586c4c7141adda4e660d
Sagar Maniya <maniyasagar26@gmail.com>
2025-09-28 16:33:07 +1000

    Set up Demo project

--- projects/demo-app/src/app/app.ts
+++ projects/demo-app/src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, JsonPipe],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('demo-app');
+  protected readonly title = signal('ng-aus-lib Demo');
+  protected readonly demoForm = new FormGroup({
+    sampleText: new FormControl<string>('', { nonNullable: true }),
+  });
+
+  protected onSubmit(): void {
+    if (this.demoForm.invalid) {
+      this.demoForm.markAllAsTouched();
+      return;
+    }
+
+    console.log('Demo form value', this.demoForm.getRawValue());
+  }
 }
